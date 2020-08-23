1844416243b63a6b8cd78032d67813a8a9070adc
wsf <17602115499@163.com>
2020-08-23 14:20:50 +0800

    签到跨行问题

--- app/src/main/java/com/example/playandroidwork/activity/DemoActivity.java
+++ app/src/main/java/com/example/playandroidwork/activity/DemoActivity.java
@@ -1,11 +1,18 @@
 package com.example.playandroidwork.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.blankj.utilcode.util.ConvertUtils;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.viewholder.BaseViewHolder;
 import com.example.playandroidwork.R;
 import com.example.playandroidwork.TabEntity;
 import com.flyco.tablayout.CommonTabLayout;
@@ -13,11 +20,15 @@ import com.flyco.tablayout.listener.CustomTabEntity;
 import com.flyco.tablayout.listener.OnTabSelectListener;
 import com.google.android.material.tabs.TabLayout;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class DemoActivity extends AppCompatActivity {
 
-    private ArrayList<CustomTabEntity> mTabEntities     = new ArrayList<>();
+    private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +83,7 @@ public class DemoActivity extends AppCompatActivity {
         commonTabLayout.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelect(int position) {
+
             }
 
             @Override
@@ -80,6 +92,48 @@ public class DemoActivity extends AppCompatActivity {
             }
         });
 
+        ArrayList<String> data = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            data.add("测试: " + i);
+        }
+
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 4);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        TestAdapter adapter = new TestAdapter(data);
+        recyclerView.setAdapter(adapter);
+
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                Log.e("wsf", position + "  " + data.size());
+                return position == data.size() - 1 ? 2 : 1;
+            }
+        });
+    }
+
+
+    public class TestAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
+
+        public TestAdapter(@Nullable List<String> data) {
+            super(R.layout.test_adapter_item_layout, data);
+        }
+
+        @Override
+        protected void convert(@NotNull BaseViewHolder holder, String s) {
+            GridLayoutManager.LayoutParams layoutParams = (GridLayoutManager.LayoutParams) holder.getView(R.id.ll_view).getLayoutParams();
+//            if (holder.getAdapterPosition() == getData().size()-1) {
+//                layoutParams.width = ConvertUtils.dp2px(142);
+//                layoutParams.height = ConvertUtils.dp2px(80);
+//            } else {
+//                layoutParams.width = ConvertUtils.dp2px(63);
+//                layoutParams.height = ConvertUtils.dp2px(80);
+//            }
+
+            if (holder.getAdapterPosition() != 0 && holder.getAdapterPosition() % 3 == 0) {
+                layoutParams.rightMargin = 0;
+            }
+        }
     }
 
 
