1b2f6e2244f84630775c442a754f2a2ea72a2dc1
wangsifeng <GxrAZk8Yo27XaiJB>
2020-08-28 16:47:32 +0800

    fix

--- app/src/main/java/com/example/playandroidwork/activity/DemoActivity.java
+++ app/src/main/java/com/example/playandroidwork/activity/DemoActivity.java
@@ -1,17 +1,14 @@
 package com.example.playandroidwork.activity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.blankj.utilcode.util.ConvertUtils;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.viewholder.BaseViewHolder;
 import com.example.playandroidwork.R;
